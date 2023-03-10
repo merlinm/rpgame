53e09ae82f9801a8a0430bfb3f472d65aba5452a
kmundaray <kmundaray@gmail.com>
2023-03-10 10:08:45 -0600

    3.10.2023

--- contacts.py
+++ contacts.py
@@ -5,8 +5,20 @@ from asciimatics.screen import Screen
 from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
 import sys
 import sqlite3
+from sqlalchemy.engine import URL, create_engine
 
-#_________________________Database Model________________________#
+#_________________________Database Engine________________________#
+connection_url = URL.create(
+    "postgresql+psycopg2",
+    username="student",
+    password="rpstudent",
+    host="rpgame.mooo.com",
+    database="student"
+    )
+
+engine = create_engine(connection_url)
+
+#_______________________UI Database Model______________________#
 class ContactModel(object):
     def __init__(self):
         # Create a database in RAM.
@@ -67,7 +79,7 @@ class LoginView(Frame):
         super(LoginView, self).__init__(screen,
                                        screen.height * 2 // 3,
                                        screen.width * 2 // 3,
-                                       on_load=self._reload_list,
+                                       #on_load=self.reset,
                                        hover_focus=True,
                                        can_scroll=False,
                                        title="RealPage Game")
@@ -96,13 +108,20 @@ class LoginView(Frame):
 
     def _log_in(self):
         self.save()
-        print("YOURE LOGGED IN")
+        with engine.connect() as connection:
+            result = connection.execute(
+                f"SELECT * FROM player WHERE playername = '{self.data['username']}' AND password_raw = '{self.data['password']}';"
+                )
+            print(result)
+            if len(result.all()) == 0:
+                connection.execute(
+                f"INSERT INTO PLAYER (playername, password_raw) VALUES ('{self.data['username']}', '{self.data['password']}');"
+                )
         raise NextScene("Main")
-
-    def _reload_list(self, new_value=None):
-        self._list_view.options = self._model.get_summary()
-        self._list_view.value = new_value
-
+    
+    def reset(self):
+        # Do standard reset to clear out form
+        super(LoginView, self).reset()
 
     @staticmethod
     def _exit():
--- rpgame.py
+++ rpgame.py
@@ -2,30 +2,33 @@
 from sqlalchemy.engine import URL, create_engine
 # from sqlalchemy.sql import table, column, select
 from sqlalchemy import text
+#import asciimatics 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
 
 def main():
 
-    connection_url = URL.create(
-        "postgresql+psycopg2",
-        username="student",
-        password="rpstudent",
-        host="rpgame.mooo.com",
-        database="student"
-        )
+    # connection_url = URL.create(
+    #     "postgresql+psycopg2",
+    #     username="student",
+    #     password="rpstudent",
+    #     host="rpgame.mooo.com",
+    #     database="student"
+    #     )
 
-    engine = create_engine(connection_url)
+    # engine = create_engine(connection_url)
 
-    with engine.connect() as connection:
-        result = connection.execute(text("select * from command;"))
-        for each in result:
-            print(each)
+    # with engine.connect() as connection:
+    #     result = connection.execute(text("select * from command;"))
+    #     for each in result:
+    #         print(each)
     
-    print("Success!")
+    # print("Success!")
 
-
-if __name__ == "__main__":
-    main()
-
+    app.run(debug=True)
 
 #HOMEWORK
 # Get the login screen to work
@@ -37,3 +40,12 @@ if __name__ == "__main__":
 # Ascii framework library > asciimatics
 # Try to get a screen working 
 # Idea to have a box where the SQL database will return the data already formatted like it does in the terminal /console
+
+
+def home():
+    return "Hello, World!"
+
+
+
+if __name__ == "__main__":
+    main()
