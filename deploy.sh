5e9fbb88b512e5d375f06889797beecfebbd8f26
Merlin Moncure <mmoncure@gmail.com>
2023-12-29 11:18:57 -0600

    fixed deploy to kill and start server process

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-#!/bin/bash
-
 git pull
 pkill streamlit
+#sorcery!
+kill `ps -axf | grep "CALL Main" | grep -v grep | cut -d ' ' -f 2`
 echo sleep to allow for application to stop
 sleep 3
 echo starting streamlit
 nohup streamlit run RPGameMain.py &
+nohup bash server.sh &
